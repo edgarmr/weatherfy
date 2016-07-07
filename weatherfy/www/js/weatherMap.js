305738164ee3e3d7f1f7b5b74851420115be6847
AlanEsan <alan.espya@gmail.com>
2016-07-07 18:05:17 -0500

    Comencé a esqueletar el index y empaté los datos que vienen del js, centrado de iconos de la toolbar

--- weatherfy/www/js/weatherMap.js
+++ weatherfy/www/js/weatherMap.js
@@ -6,14 +6,22 @@ $(document).ready(function(){
             $.get('http://api.openweathermap.org/data/2.5/weather?q='+location+'&APPID=914732d37fb626966c5bf9cf647e3342',
                   function(wtherObject){
                     var name= wtherObject.name;
-                    var photo="<img src='http://openweathermap.org/img/w/"+wtherObject.weather[0].icon+".png' width='40%'>";
+                    var photo="<img src='http://openweathermap.org/img/w/"+wtherObject.weather[0].icon+".png' width='50%'>";
                     var description = wtherObject.weather[0].description;
                     var temp = wtherObject.main.temp;
                     var tempMax = wtherObject.main.temp_max;
                     var tempMin = wtherObject.main.temp_min;
                     var wind = wtherObject.wind.speed;
                     var humidity = wtherObject.main.humidity;
-                    $("#swiper1").html("Nombre: "+name+" Foto: "+photo+" description: "+description+" temperatura: "+temp+" teperatura máxima: "+tempMax+" temperatura miníma: "+tempMin+" viento: "+wind+" humedad: "+humidity);
+                    //$("#swiper1").html("Nombre: "+name+" Foto: "+photo+" description: "+description+" temperatura: "+temp+" teperatura máxima: "+tempMax+" temperatura miníma: "+tempMin+" viento: "+wind+" humedad: "+humidity);
+                    $("#swiper1 .tarjeta .titulo h1").html(name);
+                    $("#swiper1 .tarjeta .icono").html(photo);
+                    $("#swiper1 .tarjeta .temp").html(temp);
+                    $("#swiper1 .tarjeta .desc").html(description);
+                    $("#swiper1 .tarjeta .tmax p").html(tempMax);
+                    $("#swiper1 .tarjeta .tmin p").html(tempMin);
+                    $("#swiper1 .tarjeta .viento p").html(wind);
+                    $("#swiper1 .tarjeta .humedad p").html(humidity);
                 }); 
         }else{
             alert('Inserte una localidad');
