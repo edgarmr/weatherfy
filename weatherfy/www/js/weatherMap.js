036bdbf2f415a867ea0bb8b5c94175fd4f3f9888
unknown <edgar_mdz94@gmail.com>
2016-07-08 16:45:18 -0500

    Error cuando no hay internet.

--- weatherfy/www/js/weatherMap.js
+++ weatherfy/www/js/weatherMap.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
         var location=$("#localidad").val();
         if(location!=""){
             //Obtenemos los datos relevantes del clima del día actual.
-            $.get('http://api.openweathermap.org/data/2.5/weather?q='+location+'&APPID=914732d37fb626966c5bf9cf647e3342',
-                  function(wtherObject){
+            $.get('http://api.openweathermap.org/data/2.5/weather?q='+location+'&APPID=914732d37fb626966c5bf9cf647e3342').done(
+            function(wtherObject){
                     var name= wtherObject.name;
                     var photo="<img src='http://openweathermap.org/img/w/"+wtherObject.weather[0].icon+".png' width='40%'>";
                     var description = wtherObject.weather[0].description;
@@ -14,7 +14,9 @@ $(document).ready(function(){
                     var wind = wtherObject.wind.speed;
                     var humidity = wtherObject.main.humidity;
                     $("#swiper1").html("Nombre: "+name+" Foto: "+photo+" description: "+description+" temperatura: "+temp+" teperatura máxima: "+tempMax+" temperatura miníma: "+tempMin+" viento: "+wind+" humedad: "+humidity);
-                }); 
+                }).fail(function(){
+                    alert("No hay conexión a internet");
+                });
         }else{
             alert('Inserte una localidad');
         }
