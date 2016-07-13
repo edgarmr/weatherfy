7c88330eb9f8121ba07da1fe549e789d7b8e3d45
Edgarmr <edgar@codify.com.mx>
2016-07-13 17:23:53 -0500

    Guarda los links, en el panel izquierdo y su información y manejo de errores en ajax.

--- weatherfy/www/js/weatherMap.js
+++ weatherfy/www/js/weatherMap.js
@@ -3,28 +3,36 @@ var dias = new Array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes",
 var f = new Date();
 
 $(document).ready(function(){
+    
+     for(var k=1; k<=localStorage.length; k++){
+         var id = k.toString();
+         var liSaved = localStorage.getItem(id);
+             liSaved = JSON.parse(liSaved);
+         $("#lista").append(liSaved.item);
+         
+     }
      //Acciona el mismo evento para el boton de busqueda y guardado.
      $("#search, #saveLocation").on('click',function(e){
-        var location=$("#localidad").val();
+        var location=$("#localidad").val().toUpperCase();
         if(location!=""){
             //Obtenemos los datos relevantes del clima del día actual.
             $.get('http://api.openweathermap.org/data/2.5/weather?q='+location+'&APPID=914732d37fb626966c5bf9cf647e3342').done(
                 function(wtherObject){
                     var name= wtherObject.name;
-                    var photo="<img src='http://openweathermap.org/img/w/"+wtherObject.weather[0].icon+".png' width='50%'>";
+                    var photo="<img src='http://openweathermap.org/img/w/"+wtherObject.weather[0].icon+".png'>";
                     var description = wtherObject.weather[0].description;
-                    var temp = wtherObject.main.temp;
-                    var tempMax = wtherObject.main.temp_max;
-                    var tempMin = wtherObject.main.temp_min;
+                    var temp = (parseInt(wtherObject.main.temp)-273);
+                    var tempMax = (parseInt(wtherObject.main.temp_max)-273);
+                    var tempMin = (parseInt(wtherObject.main.temp_min)-273);
                     var wind = wtherObject.wind.speed;
                     var humidity = wtherObject.main.humidity;
                     var date = dias[f.getDay()]+', '+f.getDate()+' de '+meses[f.getMonth()]+' del '+f.getFullYear();
                     $("#titulop").html(name);
                     $("#iconop").html(photo);
-                    $("#tempep").html(temp);
+                    $("#tempep").html(temp+'<sup>°</sup>');
                     $("#descp").html(description);
-                    $("#tempMaxp").html(tempMax);
-                    $("#tempMinp").html(tempMin);
+                    $("#tempMaxp").html(tempMax+'<sup>°</sup>');
+                    $("#tempMinp").html(tempMin+'<sup>°</sup>');
                     $("#vientop").html(wind);
                     $("#humedadp").html(humidity);
                     $("#fechap").html(date);
@@ -33,31 +41,52 @@ $(document).ready(function(){
                     if(e.target.id=="saveLocation"){
                         var count = parseInt($("#lista li").size());
                         count++;
-                        $("#lista").append("<li id='item"+count+"'>"
-                                              +"<a class='button button-round item'>"
-                                                +location
-                                              +"</a>" 
-                                          +"</li>");
-                        localStorage.setItem(location+"Name",name.toString());
-                        localStorage.setItem(location+"Photo",photo.toString());
-                        localStorage.setItem(location+"Description",description.toString());
-                        localStorage.setItem(location+"Temp",temp.toString());
-                        localStorage.setItem(location+"TempMax",tempMax.toString());
-                        localStorage.setItem(location+"TempMin",tempMin.toString());
-                        localStorage.setItem(location+"Wind",wind.toString());
-                        localStorage.setItem(location+"Humidity",humidity.toString());
-                        localStorage.setItem(location+"Date",date.toString());
+                        var newItem = "<li id='"+count+"' class='item'>"
+                                         +"<a class='button button-round'>"
+                                            +location
+                                         +"</a>" 
+                                     +"</li>";
+                        
+                        $("#lista").append(newItem);
+                        
+                        var locacionJson = {
+                            item: newItem.toString(),
+                            name: name.toString(),
+                            photo: photo.toString(),
+                            description: description.toString(),
+                            temp: temp.toString(),
+                            tempMax: tempMax.toString(),
+                            tempMin: tempMin.toString(),
+                            wind: wind.toString(),
+                            humidity: humidity.toString(),
+                            date: date.toString()
+                        }
+                        
+                        var locationString = JSON.stringify(locacionJson);
+                        var key = count.toString();
+                        localStorage.setItem(key,locationString);
+                        
                     }
-            }).fail(function(){
-                alert("No hay conexión a internet");
+            }).fail(function(jqXHR, textStatus, errorThrown){
+                if(jqXHR.status === 0){
+                    alert('Verifique su conexión a internet.');
+                }
+                if(jqXHR.status == 404){
+                    alert('La localidad que introdujo, no se encontró.');
+                }
+
+                if(textStatus === 'timeout'){
+                    alert('El tiempo de espera se ha agotado.');
+                }
+
             });
             //Obtenemos el clima de los siguientes 5 dias.
             $.get('http://api.openweathermap.org/data/2.5/forecast?q='+location+'&APPID=914732d37fb626966c5bf9cf647e3342',
                   function(forecast){
                     var inc=0;
                     for(var i=1; i<=5; i++){
-                        var ftmin = forecast.list[1+inc].main.temp_min;
-                        var ftmax = forecast.list[1+inc].main.temp_max;
+                        var ftmin = (parseInt(forecast.list[1+inc].main.temp_min)-273);
+                        var ftmax = (parseInt(forecast.list[1+inc].main.temp_max)-273);
                         var desc = forecast.list[1+inc].weather[0].description;
                         var icon1 = "<img src='http://openweathermap.org/img/w/"+forecast.list[1+inc].weather[0].icon+".png'>";
                         var wind = forecast.list[1+inc].wind.speed;
@@ -79,21 +108,18 @@ $(document).ready(function(){
      });
     //Agrega el evento a todos los elementos de la lista y muestra su informacion dependiendo de la ciudad, mediante localStorage.
     $(document).on('click','.item',function(){
-        var loc = $(this).html().toString();
-        $("#titulop").html(localStorage.getItem(loc+"Name"));
-        $("#iconop").html(localStorage.getItem(loc+"Photo"));
-        $("#tempep").html(localStorage.getItem(loc+"Description"));
-        $("#descp").html(localStorage.getItem(loc+"Temp"));
-        $("#tempMaxp").html(localStorage.getItem(loc+"TempMax"));
-        $("#tempMinp").html(localStorage.getItem(loc+"TempMin"));
-        $("#vientop").html(localStorage.getItem(loc+"Wind"));
-        $("#humedadp").html(localStorage.getItem(loc+"Humidity"));
-        $("#fechap").html(localStorage.getItem(loc+"Date"));
+        var key = $(this).attr("id").toString();
+        var locacionGuardada = localStorage.getItem(key);
+        
+        locacionGuardada = JSON.parse(locacionGuardada);
+        $("#titulop").html(locacionGuardada.name);
+        $("#iconop").html(locacionGuardada.photo);
+        $("#tempep").html(locacionGuardada.description);
+        $("#descp").html(locacionGuardada.temp);
+        $("#tempMaxp").html(locacionGuardada.tempMax);
+        $("#tempMinp").html(locacionGuardada.tempMin);
+        $("#vientop").html(locacionGuardada.wind);
+        $("#humedadp").html(locacionGuardada.humidity);
+        $("#fechap").html(locacionGuardada.date);
     });
-});
-
-
-                  
-                  
-    
-                  
+});                  
